85dbe48f1768b7126fb15a0a3858ace3b7f21d96
killiandonnegan <86739456+killiandonnegan@users.noreply.github.com>
2023-10-24 16:24:04 +0100

    Updated libraries

--- download.py
+++ download.py
@@ -1,23 +1,15 @@
-from cgitb import text
-from email.mime import audio
-from importlib.resources import path
-from msilib.schema import Error
-import shutil
-from tkinter import *
-from tkinter import filedialog
-from tkinter import font
-from tkinter.messagebox import showinfo
-from turtle import color, down
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from numpy import place
-from pytube import YouTube
-from tkinter import messagebox
 import os
-import webbrowser
 import sys
+import shutil
+import webbrowser
+from tkinter import *
+from tkinter import filedialog, messagebox
+
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from pytube import YouTube
+
 
 #Functions
-
 
 def get_path(filename):
     if hasattr(sys, "_MEIPASS"):
@@ -28,17 +20,16 @@ def get_path(filename):
 
 def select_path():
     path = filedialog.askdirectory()
-
     path_label.config(text=path)
 
 
-def download_mp4():
+def download():
         try:
             #get link
             get_link = link_field.get()
 
             if get_link=="":  #check if there is something in link field
-                messagebox.showinfo(title="klgrm.xyz", message="Enter a Youtube link.")
+                messagebox.showinfo(title="Youtube Downloader", message="Enter a Youtube link.")
 
             else:
                 #get selected path
@@ -56,7 +47,7 @@ def download_mp4():
                             new_file = base + '.mp3'
                             os.rename(videofile, new_file)
                             shutil.move(new_file, user_path)
-                            messagebox.showinfo(title="klgrm.xyz", message="Your "+form.get()+" file has been downloaded to "+user_path)
+                            messagebox.showinfo(title="Youtube Downloader", message="Your "+form.get()+" file has been downloaded to "+user_path)
                             
                             
                     #if user wants mp4
@@ -69,29 +60,28 @@ def download_mp4():
 
                     #they have not selected either
                     else:
-                            messagebox.showinfo(title="klgrm.xyz", message="Select a file format.")
+                            messagebox.showinfo(title="Youtube Downloader", message="Select a file format.")
                 
                 else: #path has not been selected
-                    messagebox.showinfo(title="klgrm.xyz", message="Select your download path.")
+                    messagebox.showinfo(title="Youtube Downloader", message="Select your download path.")
 
         except: #only other error is invalid url
-            messagebox.showinfo(title="klgrm.xyz", message="You have entered an invalid URL.")
+            messagebox.showinfo(title="Youtube Downloaderz", message="You have entered an invalid URL.")
             
 
 def callback(url):
    webbrowser.open_new_tab(url)
 
+#UI
 
 screen = Tk()
-screen.iconbitmap(get_path("yt.ico"))
+screen.iconbitmap(get_path("images/yt.ico"))
 title = screen.title("Youtube Downloader")
 canvas = Canvas(screen, width=500, height=600, background="#262626")
 
-
-#logo, and resize using subsample
-logo = PhotoImage(file=get_path("yt.png")).subsample(3, 3)
+#Logo, and resize using subsample
+logo = PhotoImage(file=get_path("images/yt.png")).subsample(3, 3)
 canvas.create_image(250, 130, image=logo)
-
 
 #Enter video link
 link_label = Label(screen, text="Enter the link of your video: ", font=('Consolas', 15), bg="#262626", fg="white").place(relx=0.5, rely=0.45, anchor=CENTER)
@@ -100,7 +90,6 @@ link_field.pack()
 link_field.place(relx=0.5, rely=0.5, anchor=CENTER)
 canvas.create_window(250, 270, window=link_label)
 canvas.create_window(250, 300, window=link_field)
-
 
 #Button to select path
 path_label = Label(screen, text="Path: ", bg="#262626", fg="white")
@@ -111,7 +100,6 @@ path_button = Button(screen, text="Edit Path", font=('Consolas', 15), command=se
 canvas.create_window(250, 340, window=path_label)
 canvas.create_window(250, 370, window=path_button)
 
-
 #Select file format/extension
 format_options = [".mp3", ".mp4"]
 form = StringVar(screen)
@@ -121,23 +109,22 @@ format_menu.pack()
 format_menu.config(font=('Consolas', 15))
 format_menu.place(relx=0.5, rely=0.75, anchor=CENTER)
 
-
 #Download button
-download = Button(screen, text="Download", command=download_mp4, bg="green", font=('Consolas', 15)).place(relx=0.5, rely=0.85, anchor=CENTER)
+download = Button(screen, text="Download", command=download, bg="green", font=('Consolas', 15)).place(relx=0.5, rely=0.85, anchor=CENTER)
 canvas.create_window(250, 410, window=download)
 
-
 #Footer
-footer = Label(text="klgrm.xyz © 2022", bg="#262626", fg="white", font=('Consolas', 15, UNDERLINE))
+footer = Label(text="Killian Donnegan", bg="#262626", fg="white", font=('Consolas', 15, UNDERLINE))
 footer.pack()
 footer.place(relx=0.5, rely=0.95, anchor=CENTER)
 footer.bind("<Button-1>", lambda e:
-callback("http://klgrm.xyz"))
+callback("https://github.com/killiandonnegan"))
 
 
 #Widgets will resize when window adjusted
 canvas.pack(fill="both", expand=True)
 
+#Fixed size
 screen.minsize(500, 600)
 screen.maxsize(500, 600)
 screen.mainloop()
